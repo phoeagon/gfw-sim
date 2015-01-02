be55965e40375fe8ae5bf5c20b6467a9aaf2606b
phoeagon <phoeagon@gmail.com>
2015-01-02 15:42:11 +0800

    revert updater.sh

--- updater/updater.sh
+++ updater/updater.sh
@@ -2,13 +2,13 @@
 DEST_PACKAGE="/tmp/gfw.update.tgz"
 cd /tmp || exit
 # Get update digest
-URL="http://cdn.rawgit.com/phoeagon/gfw-sim/release/releases/digest"
-wget ${URL} -O /tmp/gfw.digest
+URL="http://raw.githubusercontent.com/phoeagon/gfw-sim/release/releases/digest"
+wget --no-check-certificate ${URL} -O /tmp/gfw.digest
 # Test if need to update
 diff /etc/gfw.digest /tmp/gfw.digest && exit
 PACKAGE=`sed '2q;d' /tmp/gfw.digest`
 # Download.
-wget ${PACKAGE} -O ${DEST_PACKAGE}
+wget --no-check-certificate ${PACKAGE} -O ${DEST_PACKAGE}
 # Check checksum
 EXPECTED_DIGEST=`head -1 /tmp/gfw.digest`
 echo ${EXPECTED_DIGEST} ${PACKAGE} | md5sum -c || exit
