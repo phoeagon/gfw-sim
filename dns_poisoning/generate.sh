c5257d12f9463f6bb331ec72d0f286813ea3b741
phoeagon <phoeagon@gmail.com>
2015-02-04 17:01:45 +0800

    added BRICKS from github to blocked domain list

--- dns_poisoning/generate.sh
+++ dns_poisoning/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 DEAD_IPS=`wget -O - 'https://gist.github.com/felixonmars/7ce1dcd193c65937e670/raw/f75bce4a0f9420f08cedad26477f98b5fbc0ec2e/dns_pollution_iplist.txt'`
-BLACKLIST=`cat blacklist.txt`
+wget -O /tmp/bl.txt https://raw.githubusercontent.com/Leask/BRICKS/master/gfw.bricks
+BLACKLIST=`cat /tmp/bl.txt blacklist.txt | sort | uniq`
 
 echo "# Blacklisted."
 for domain in $BLACKLIST
@@ -10,13 +11,6 @@ for domain in $BLACKLIST
   done
 
 
-# New Source
-BLACKLIST=`wget -O - https://raw.githubusercontent.com/Leask/BRICKS/master/gfw.bricks`
-for domain in $BLACKLIST
-  do
-    DEAD=`echo $DEAD_IPS | tr ' ' '\n' | sort -R | head -1`
-    echo "address=/"${domain}"/"${DEAD}
-  done
 
 echo "# Otherwise, go to baidu public DNS."
 echo "server=/#/180.76.76.76#53"
